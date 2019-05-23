c9af4c7095f51f04bdb9fe981b6b3d68b1fe4978
hzzhaolong <hzzhaolong@corp.netease.com>
2019-05-23 10:29:23 +0800

    测试工程

---
+++ src/main/java/com/java/StringTest.java
@@ -0,0 +1,12 @@
+package com.java;
+
+/**
+ * Created by hzzhaolong on 19/5/23.
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+        System.out.println("main method");
+    }
+
+}
